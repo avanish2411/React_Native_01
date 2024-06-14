b07b3fb8125ae4982e403a9c199ef7f62ff4bca0
avanish2411 <avanishsingh2411.com>
2024-06-14 12:36:35 +0530

    added drawer navigation

---
+++ src/Navigation/AppNavigation.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+
+import HomeScreen from '../Screens/HomeScreen';
+import ProfileScreen from '../Screens/ProfileScreen';
+
+const Drawer = createDrawerNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
